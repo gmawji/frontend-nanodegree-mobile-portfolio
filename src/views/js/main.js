51ccbdd79a2087230703e3f247283fc05d5c39ae
gmawji <git@gmawji.com>
2017-06-05 03:47:45 -0400

    Resubmit fixes.

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -526,12 +526,15 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  var rows = (window.screen.height / s) * cols;
+  console.log(rows);
+
+  for (var i = 0; i < rows; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
-    elem.style.height = "70px";
-    elem.style.width = "50px";
+    elem.style.height = "100px";
+    elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.getElementById("movingPizzas1").appendChild(elem);
